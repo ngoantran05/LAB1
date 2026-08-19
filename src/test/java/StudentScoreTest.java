433c5ee0e7183348dc6193e526ab4049520d3c5c
ngoantran05 <23010154@st.phenikaa-uni.edu.vn>
2026-08-19 11:11:01 +0700

    Add JUnit tests for statement coverage (#1)

---
+++ src/test/java/StudentScoreTest.java
@@ -0,0 +1,26 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class StudentScoreTest {
+
+    @Test
+    void testExcellentScore() {
+        int[] scores = {9, 8, 10};
+
+        assertEquals("Excellent", StudentScore.classifyScore(scores));
+    }
+
+    @Test
+    void testPassScore() {
+        int[] scores = {6, 7, 5};
+
+        assertEquals("Pass", StudentScore.classifyScore(scores));
+    }
+
+    @Test
+    void testFailScore() {
+        int[] scores = {3, 4, 2};
+
+        assertEquals("Fail", StudentScore.classifyScore(scores));
+    }
+}
